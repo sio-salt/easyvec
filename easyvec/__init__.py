e114a7aed60cca4b2a657f2d7cbc7d5d51fd7a40
sio-salt <saltinthedesertyou@gmail.com>
2024-12-12 00:00:55 +0900

    modified init

--- easyvec/__init__.py
+++ easyvec/__init__.py
@@ -1,3 +1,3 @@
-from .vec3 import Vec3, vec_type, mat_type
+from .vec3 import Vec3, vec_type, mat_type, vectorize_arg
 
-__all__ = ["Vec3", "vec_type" "mat_type"]
+__all__ = ["Vec3", "vec_type" "mat_type", "vectorize_arg"]
